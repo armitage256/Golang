a93b0f22be84499e0ad86db2b8bedd052a7ca0c6
armitage256 <sexeymitagge@gmail.com>
2018-12-23 19:25:56 -0200

    Como Programar Web com Golang - HTML - Condicionais EQ NE

---
+++ Video-36/main.go
@@ -0,0 +1,38 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"html/template"
+	"strings"
+)
+
+var templates *template.Template
+
+func main() {
+
+	fmt.Println("Listening port 8080")
+
+	templates = template.Must( template.ParseGlob("templates/*.html") )
+
+	http.HandleFunc("/", index)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+
+	name := r.PostForm.Get("name")
+
+	name = strings.TrimSpace(name)
+
+	templates.ExecuteTemplate(w, "index.html", struct{
+		Name string
+	}{
+		Name: name,
+	})
+
+}
