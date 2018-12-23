ed76bb3794661a7a2b2a90ad3cdff9879d02f09e
armitage256 <sexeymitagge@gmail.com>
2018-12-23 18:24:27 -0200

    Como Programar Web com Golang - Formulários [POST]

---
+++ Video-34/main.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+var templates *template.Template
+
+func main() {
+
+	fmt.Println("Listening port 8080")
+
+	templates = template.Must( template.ParseGlob("templates/*.html") )
+
+	http.HandleFunc("/", index)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+
+	name := r.PostForm.Get("name")
+
+	//fmt.Printf( "Seja bem vindo! %s\r\n" ,name)
+
+	templates.ExecuteTemplate(w, "index.html", struct{
+		Name string
+	}{
+		Name: name,
+	})
+
+}
